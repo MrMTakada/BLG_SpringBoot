06f61850230febfc7cc12f6a68ca4eb136fdf2aa
MrMTakada <misato@marineroad.com>
2017-10-22 14:52:54 +0900

    Thymeleaf応用編

--- src/main/java/com/marineroad/springboot/ThymeleafController.java
+++ src/main/java/com/marineroad/springboot/ThymeleafController.java
@@ -1,5 +1,7 @@
 package com.marineroad.springboot;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,10 +19,25 @@ public class ThymeleafController {
 	@RequestMapping("/thdemo")
 	public ModelAndView index(ModelAndView mav) {
 
-		// 変数式
+		// 1.(1) 変数式
 		// ・コントローラーからテンプレートに値を渡す
 		// ・変数「msg」に値を設定
-		mav.addObject("msg", "コントローラーからテンプレートに値を渡す"); // (2)
+		mav.addObject("msg", "コントローラーからテンプレートに値を渡す");
+
+		// 1.(2) 選択変数式
+		// ・オブジェクトの中にあるプロパティ（フィールド）を取り出すためのもの
+		// ・変数「object」に値を設定
+		DataObject obj = new DataObject(123, "マリンロード", "https://www.marineroad.com/");
+		mav.addObject("object", obj);
+
+		// 2.(1) 繰り返し
+		// ・配列やコレクションから繰り返し値を取り出すためのもの
+		// ・変数「dataList」に値を設定
+		ArrayList<DataObject> dataList = new ArrayList<DataObject>();
+		dataList.add(new DataObject(1, "マリンロード", "https://www.marineroad.com/"));
+		dataList.add(new DataObject(2, "ヤフー", "https://www.yahoo.co.jp/"));
+		dataList.add(new DataObject(3, "グーグル", "https://www.google.co.jp/"));
+		mav.addObject("dataList", dataList);
 
 		// 使用するビューを設定
 		mav.setViewName("thdemo"); // (3)
